b0cb79f6b1085b4eb9302b87ca835dd0193efca8
bhavin0810 <bhavin0810@gmail.com>
2015-02-20 22:47:07 -0500

    add getters and setters and Override toString Method in Planet Class

--- src/Planet.java
+++ src/Planet.java
@@ -2,8 +2,8 @@
  * @author: Bhavin Patel
  * @Description: Implementation Of the Planet Abstract Class with necessary properties and methods
  * @version 1.0  Feb-20-2015 Creating the Planet Class with necessary Properties and constructor
- * 
- *  */
+ * @version 2.0 Feb-20-2015 Creating Getters and Setters and Override toString Method
+ * *  */
 public abstract class Planet {
 	
 	//private Variables
@@ -15,10 +15,64 @@ public abstract class Planet {
 	private int ringCount;
 	private double rotationPeriod;
 
+	//constructor
 	public Planet(double diameter, double mass, String name) {		
 		this.diameter = diameter;
 		this.mass = mass;
 		this.name = name;
+	}
+	
+	//Getters and Setters  for the Private Properties
+	public int getMoonCount() {
+		return moonCount;
+	}
+
+	public void setMoonCount(int moonCount) {
+		this.moonCount = moonCount;
+	}
+
+	public double getOrbitalPeriod() {
+		return orbitalPeriod;
+	}
+
+	public void setOrbitalPeriod(double orbitalPeriod) {
+		this.orbitalPeriod = orbitalPeriod;
+	}
+
+	public int getRingCount() {
+		return ringCount;
+	}
+
+	public void setRingCount(int ringCount) {
+		this.ringCount = ringCount;
+	}
+
+	public double getRotationPeriod() {
+		return rotationPeriod;
+	}
+
+	public void setRotationPeriod(double rotationPeriod) {
+		this.rotationPeriod = rotationPeriod;
+	}
+
+	public double getDiameter() {
+		return diameter;
+	}
+
+	public double getMass() {
+		return mass;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+
+	//toString override method
+	@Override
+	public String toString() {
+		return "Planet [diameter=" + diameter + ", mass=" + mass + ", name="
+				+ name + "]";
 	}
 	
 }
